523a0eaa6e425b3812bb8d6aecd81e62e5b2fcf9
sambantham <76392855+sambantham-p@users.noreply.github.com>
2023-03-13 00:03:55 +0530

    Update Address.js

--- Address.js
+++ Address.js
@@ -1,9 +1,9 @@
 import {View,StyleSheet,Text} from "react-native"
 import React,{useState} from "react"
 let bip39 = require('bip39') 
-
+let [add,setAdd] = useState();
 export default function Address(){
-    let [add,setAdd] = useState();
+   
     setAdd = bip39.mnemonicToSeedSync('basket actual').toString('hex')
     return(
     <View style={styles.sty}>
